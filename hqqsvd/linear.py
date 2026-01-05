6e0df2785746d7b25c26fddf6a60a6d011702fa8
jk <jonaskonschak@gmx.de>
2026-01-05 17:06:28 +0100

    add comfyui lora support

--- hqqsvd/linear.py
+++ hqqsvd/linear.py
@@ -1,7 +1,13 @@
 import torch
-torch._dynamo.config.cache_size_limit = max(8192, getattr(torch._dynamo.config, "cache_size_limit", 0))
-torch._dynamo.config.accumulated_recompile_limit = max(8192, getattr(torch._dynamo.config, "accumulated_recompile_limit", 0))
+
+torch._dynamo.config.cache_size_limit = max(
+    8192, getattr(torch._dynamo.config, "cache_size_limit", 0)
+)
+torch._dynamo.config.accumulated_recompile_limit = max(
+    8192, getattr(torch._dynamo.config, "accumulated_recompile_limit", 0)
+)
 from .quantize import quantize, dequantize
+
 
 class Lora(torch.nn.Module):
     def __init__(self, name, lora_up, lora_down, alpha, strength=1.0):
@@ -11,15 +17,46 @@ class Lora(torch.nn.Module):
         self.lora_down = lora_down
         self.alpha = alpha if alpha is not None else 1.0
         self.rank = lora_up.shape[1]
-        self.scale = torch.tensor([strength * self.alpha / self.rank], device="cuda", dtype=lora_up.dtype)
+        self.scale = torch.tensor(
+            [strength * self.alpha / self.rank], device="cuda", dtype=lora_up.dtype
+        )
         self.strength = strength
 
-    def get_weight(self):
-        return self.scale * self.lora_up @ self.lora_down
+    def get_weight(self, weight):
+        return weight + self.scale * self.lora_up @ self.lora_down
+
+
+class ComfyLora:
+    def __init__(self, name, comfy_lora, calculate_weight):
+        self.comfy_lora = comfy_lora
+        self.name = name
+        self.calculate_weight = calculate_weight
+
+    @torch._dynamo.disable
+    def get_weight(self, weight):
+        return (
+            self.calculate_weight(
+                [
+                    self.comfy_lora,
+                ],
+                weight,
+                self.name.split("|")[0],
+            )
+            - weight
+        )
+
 
 class HQQSVDLinear(torch.nn.Module):
     def __init__(
-        self, W_q, svd_up, svd_down, scale, zero_point, bias, nbits, int8_matmul:bool=True
+        self,
+        W_q,
+        svd_up,
+        svd_down,
+        scale,
+        zero_point,
+        bias,
+        nbits,
+        int8_matmul: bool = True,
     ):
         super().__init__()
         self.in_features = svd_down.shape[1]
@@ -35,10 +72,14 @@ class HQQSVDLinear(torch.nn.Module):
         self.scale = torch.nn.Parameter(scale, False)
         self.zero_point = torch.nn.Parameter(zero_point, False)
         self.bias = torch.nn.Parameter(bias, False)
-        self.nbits = torch.nn.Parameter(torch.tensor([nbits]), False) # for serialization
+        self.nbits = torch.nn.Parameter(
+            torch.tensor([nbits]), False
+        )  # for serialization
         self._nbits = nbits
         self.int8_matmul = int8_matmul
         self.loras = {}
+        self.forward_no_comfy = torch.compile(self._forward, fullgraph=True)
+        self.forward_comfy = torch.compile(self._forward)
 
     @classmethod
     def from_linear(
@@ -52,17 +93,15 @@ class HQQSVDLinear(torch.nn.Module):
         W_q, svd_up, svd_down, scale, zero_point = quantize(
             linear.weight, svd_rank, svd_steps, group_size, nbits
         )
-        return cls(
-            W_q, svd_up, svd_down, scale, zero_point, linear.bias, nbits
-        )
-    
+        return cls(W_q, svd_up, svd_down, scale, zero_point, linear.bias, nbits)
+
     def add_lora(self, lora):
         self.loras[lora.name] = lora
-    
+
     def remove_lora(self, name):
         self.loras.pop(name)
-    
-    def dequantize(self):
+
+    def dequantize(self, apply_lora=False):
         W_f = dequantize(
             self.weight,
             self.svd_up,
@@ -71,27 +110,36 @@ class HQQSVDLinear(torch.nn.Module):
             self.zero_point,
             self.q_shape,
             self.o_shape,
-            self._nbits
+            self._nbits,
         )
-        for lora in self.loras.values():
-            W_f += lora.get_weight()
+        if apply_lora:
+            for lora in self.loras.values():
+                W_f += lora.get_weight(W_f)
         return W_f
-    
-    def forward_int8(self, x:torch.FloatTensor):
+
+    def forward_int8(self, x: torch.FloatTensor):
         x = x.view((-1, x.shape[-1]))
         dtype = x.dtype
-        W_f = self.dequantize().T
+        W_f = self.dequantize(apply_lora=True).T
 
         scale_x = torch.amax(x.abs(), dim=1, keepdims=True).div_(127)
         x_q = torch.div(x, scale_x).round_().clamp_(-128, 127).to(dtype=torch.int8)
 
         scale_w = torch.amax(W_f.abs(), dim=0, keepdims=True).div_(127)
         W_q = torch.div(W_f, scale_w).round_().clamp_(-128, 127).to(dtype=torch.int8)
-        
-        return (torch._int_mm(x_q, W_q).to(dtype) * scale_x * scale_w).unsqueeze(0) + self.bias
 
-    @torch.compile(fullgraph=True)
-    def forward(self, x:torch.FloatTensor):
+        return (torch._int_mm(x_q, W_q).to(dtype) * scale_x * scale_w).unsqueeze(
+            0
+        ) + self.bias
+
+    def _forward(self, x: torch.FloatTensor):
         if self.int8_matmul and x.numel() / x.shape[-1] >= 16:
             return self.forward_int8(x)
-        return torch.nn.functional.linear(x, self.dequantize(), self.bias)
+        return torch.nn.functional.linear(
+            x, self.dequantize(apply_lora=True), self.bias
+        )
+
+    def forward(self, x: torch.FloatTensor):
+        if any([isinstance(lora, ComfyLora) for lora in self.loras.values()]):
+            return self.forward_comfy(x)
+        return self.forward_no_comfy(x)
